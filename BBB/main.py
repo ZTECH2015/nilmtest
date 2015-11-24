21643ffae1d0b69fd7c3f0abb62d76299ca3cccd
root <root@beaglebone.(none)>
2015-11-24 01:32:52 +0000

    udp test

--- BBB/main.py
+++ BBB/main.py
@@ -6,20 +6,21 @@ import Adafruit_BBIO.UART as UART
 import beaglebone_pru_adc as adc
 import serial
 from multiprocessing import Process, Queue
-from timeout import timeout
+#from timeout import timeout
 
 # Set up zooming coefficient
-offset = 5*9.1/(9.1+47)
-v_conv = 1.8/4096
-voltage_zoom = 56150/float(150)
-current_zoom = 100/0.033/28
+#offset = 5*9.1/(9.1+47)
+#v_conv = 1.8/4096
+#voltage_zoom = 56150/float(150)
+#current_zoom = 100/0.033/28
 
 #print "offset", offset
 #print "voltage_zoom", voltage_zoom
 #print "current_zoom", current_zoom
 
 # Set the socket parameters
-host = '104.194.113.209'
+#host = '104.194.113.209'
+host = '104.194.113.208'
 host_qb = '10.221.33.195'
 port = 9999
 
@@ -37,26 +38,18 @@ def readUI(q):
 		data[num_ui,0] = time.time()
 		for i in range(num_ui):
 			data[i] = capture.values[:2]
-			time.sleep(0.00001)
-		#data[:,0] = (data[:,0]*v_conv-offset) * voltage_zoom
-		#data[:,1] = (data[:,1]*v_conv-offset) * current_zoom/resist
-		#print type(data)
+			time.sleep(0.00002)
 		data[num_ui,1]=time.time()
 		q.put(data)
-		#print data
-		#print(time.time() - start)
+
 def send(q,s,addr):
 	while 1:
 		start0 = time.time()
-		# start = data[num_ui,0]
-		# end = data[num_ui,1]
-		# u_data = (data[:num_ui,0].reshape(-1,div_ui).mean(axis = 1)*v_conv-offset) * voltage_zoom
-		# i_data = (data[:num_ui,1].reshape(-1,div_ui).mean(axis = 1)*v_conv-offset) * current_zoom
-		# data = np.vstack((np.vstack((u_data, i_data)).transpose(),[start,end]))
-		#print data
 		s.sendto(pickle.dumps(q.get(True)),addr)
+		#s.send(pickle.dumps(q.get(True), protocol = -1))
+		time.sleep(0.15)
 		print("send data consume:", time.time()-start0)
-		time.sleep(0.1)
+		
 
 def readEMI(q):
 	UART.setup("UART1")
@@ -65,11 +58,11 @@ def readEMI(q):
 	ser.open()
 	if ser.isOpen():
 		while(1):
-			#print "writing uart"
-			#start = time.time()
 			ser.write('1')
-			#q.put(ser.read(4096))
-			q.put(np.fromstring(ser.read(4096), dtype = np.float32))
+			data = ser.read(4096)
+			#data = np.fromstring(ser.read(4096), dtype = np.float32)
+			q.put(data)
+			#print data.shape[0]
 			#print "finish writing"
 			#send2Server_emi(ser.read(4096))
 			#print("send EMI consume:", time.time()-start)
